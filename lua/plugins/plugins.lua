cf6afc785209e6c23d55071e5bb0dfe56d08737f
Novian Deny <anggerdeni@gmail.com>
2024-01-30 21:28:46 +0700

    chore: remove zen mode

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -68,14 +68,6 @@ return {
             -- refer to the configuration section below
         },
     },
-    {
-        'folke/zen-mode.nvim',
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
 
     -- plugins/rest.lua
     {
