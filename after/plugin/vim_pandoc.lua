bd358236074f4e70e56fd97718f02314c0a35b39
Novian Deny <anggerdeni@gmail.com>
2024-01-30 22:59:47 +0700

    chore: add comment

--- after/plugin/vim_pandoc.lua
+++ after/plugin/vim_pandoc.lua
@@ -1,1 +1,3 @@
+-- TODO: modify mapping here to make your life easier
+-- I disabled it because it messes with my <leader>b keymap which should open netrw
 vim.g['pandoc#keyboard#use_default_mappings'] = 0
